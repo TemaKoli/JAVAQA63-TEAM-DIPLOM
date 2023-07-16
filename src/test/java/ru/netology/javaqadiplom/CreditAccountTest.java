35845edc305e4eda7894312c34f2983c7a759b8b
Kozyavin V <kioci@yandex.ru>
2023-07-16 12:36:35 +0300

    AddBagReport

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -1,21 +1,50 @@
 package ru.netology.javaqadiplom;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
 
+    /////////////////////////////////////////////метод Add/////////////////////////////////////////////////////
     @Test
-    public void shouldAddToPositiveBalance() {
-        CreditAccount account = new CreditAccount(
-                0,
-                5_000,
-                15
-        );
+    public void AddWithPositiveBalanceTest() {
+        CreditAccount account = new CreditAccount(2_000, 6_000, 15);
 
         account.add(3_000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        int expected = 5_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    ////////////////////////////////////////////метод yearChange//////////////////////////////////////////////////
+    @Test
+    public void yearChangePositiveBalanceTest() {
+        CreditAccount account = new CreditAccount(1_000, 6_000, 10);
+
+        int expected = 0;
+        int actual = account.yearChange();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void yearChangeNegativeBalanceTest() {
+        CreditAccount account = new CreditAccount(-1_000, 6_000, 10);
+
+        int expected = -100;
+        int actual = account.yearChange();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void yearChangeZeroBalanceTest() {
+        CreditAccount account = new CreditAccount(0, 6_000, 10);
+
+        int expected = 0;
+        int actual = account.yearChange();
+
+        Assertions.assertEquals(expected, actual);
     }
 }
-/////////тест
